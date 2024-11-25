6ee5d4b963948a262d2f23bf5430ecb460470602
KopperIfr <kopper.private@gmail.com>
2024-11-25 14:53:22 +0400

    .
    .

--- server/app.js
+++ server/app.js
@@ -1,14 +1,28 @@
+// 1. Imports..
+
 import express from 'express';
 import dotenv from 'dotenv';
 import controller from './controllers/controller.js';
+import connectDB from './database/connection.js';
 
+
+// 2. Initializing variables..
 dotenv.config();
+connectDB();
 const app = express();
+const PORT = process.env.PORT;
 
+
+// 3. Middleware functions..
+
+
+// 4. Routes..
 app.post('/sign-in', controller.signIn);
 app.post('/sign-up', controller.signUp);
 app.post('/sign-out', controller.signOut);
 
+
+// 5. Server listening.. 
 app.listen(3002, () => {
     console.log('Server listening on port 3002..');
 });
